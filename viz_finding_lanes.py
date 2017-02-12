42264fec2a1baa24d5b8cb878d35ed1934a1018f
ddrsmiLe <ddrsmile@gmail.com>
2017-02-12 22:52:58 +0900

    completed visualization of finding lane

--- viz_finding_lanes.py
+++ viz_finding_lanes.py
@@ -12,16 +12,16 @@ from LaneFinder.line import Line
 
 # define the windows for the perspective transform
 src = np.float32([
-    [590, 450],
-    [720, 450],
-    [1115, 720],
-    [170, 720]
+    [550, 455],
+    [750, 455],
+    [1170, 720],
+    [150, 720]
 ])
 
 dst = np.float32([
     [450, 0],
-    [850, 0],
-    [850, 720],
+    [830, 0],
+    [830, 720],
     [450, 720]
 ])
 
@@ -38,7 +38,9 @@ nonzeroy = None
 
 def get_binary_warped(image):
     image = calibrator.undistort(image)
+    save_single_image(image, 'undistorted')
     warp = ptransformer.transform(image)
+    save_single_image(warp, 'perspective_transform')
     binary_warped = masker.get_masked_image(warp)
     return binary_warped
 
@@ -50,6 +52,20 @@ def get_good_inds(base, margin, y_low, y_high):
     return np.where((((base - margin) <= nonzerox)&(nonzerox <= (base + margin))&\
                     ((nonzeroy >= y_low) & (nonzeroy <= y_high))))
 
+def save_single_image(image, fname, fitted_lines=None):
+    fig = plt.figure(figsize=(4, 2.25))
+    plt.imshow(image)
+    plt.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off', labelleft='off')
+    if fitted_lines is not None:
+        ## draw fitted lines
+        plt.plot(fitted_lines['l_x'], fitted_lines['ploty'], color='yellow')
+        plt.plot(fitted_lines['r_x'], fitted_lines['ploty'], color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+    plt.savefig('output_images/viz/viz_{}.png'.format(fname))
+    #plt.show()
+    
+
 def histogram_detection(viz_img, image, search_area, steps, margin=25):
     # setup targeted image for searching lane
     target_img = image[:, search_area[0]:search_area[1]]
@@ -59,24 +75,34 @@ def histogram_detection(viz_img, image, search_area, steps, margin=25):
     x = np.array([], dtype=np.float32)
     y = np.array([], dtype=np.float32)
 
+    last_base = None
+
     for i in range(steps):
         # define the range in y direction for searching
         end = target_img.shape[0] - (i * px_per_step)
         start = end - px_per_step
 
-        histogram = np.sum(target_img[start:end, :], axis=0)
-        # add search_area[0], image offset in x direction, 
-        # to ensure the positions of points are correct.
-        base = np.argmax(histogram) + search_area[0]
+        if last_base is None:
+            histogram = np.sum(target_img[start:end, :], axis=0)
+            # add search_area[0], image offset in x direction, 
+            # to ensure the positions of points are correct.
+            base = np.argmax(histogram) + search_area[0]
+        else:
+            base = last_base
         # draw searching window
         cv2.rectangle(viz_img, (base-margin,start),(base+margin, end),(255,125,0), 2)
         # get the indices in the searching area based on "base" and "margin"
         good_inds = get_good_inds(base, margin, start, end)
+        cur_x, cur_y = nonzerox[good_inds], nonzeroy[good_inds]
 
         # append x and y if there are points found gotten by good indices
-        if np.sum(nonzerox[good_inds]):
-            x = np.append(x, nonzerox[good_inds].tolist())
-            y = np.append(y, nonzeroy[good_inds].tolist())
+        if np.sum(cur_x):
+            x = np.append(x, cur_x.tolist())
+            y = np.append(y, cur_y.tolist())
+            if np.sum(cur_x) > 50:
+                last_base = np.int(np.mean(cur_x))
+        else:
+            last_base = None
 
     return x.astype(np.float32), y.astype(np.float32)
 
@@ -97,30 +123,25 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     image = cv2.imread('test_images/test4.jpg')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    orig_image = image.copy()
+    input_image = image.copy()
+    save_single_image(input_image, 'input_image')
 
     # get masked and warped image in 3 dimensional form
     binary_warped = get_binary_warped(image)
-    # create out_img to visualization
-    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-    #plt.imshow(out_img)
-    #plt.show()
-    
+    # create fit_img to visualize the fitting process
+    fit_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    save_single_image(fit_img, 'binary_warped')
     
 
     # draw the target area where lanefinder searches the lane
-    img_offset = np.int(dst[0][0]*0.3)
-    targeted = out_img.copy()
+    img_offset = np.int(dst[0][0]*0.8)
+    targeted = fit_img.copy()
     cv2.rectangle(targeted,(img_offset,0),(image.shape[1]//2 - 5, image.shape[0]),(255,0,0), 5)
     cv2.rectangle(targeted,(image.shape[1]//2 + 5,0),(image.shape[1] - img_offset, image.shape[0]),(0,0,255), 5)
-    #plt.imshow(targeted)
-    #plt.show()
+    save_single_image(targeted, 'search_target')
 
     # set nonzeros for searching lanes
     nonzerox, nonzeroy = set_nonzeros(binary_warped)
-    # create line objects for both left and right lane
-    left = Line()
-    right = Line()
 
     # create the containers to store current found points
     l_x = l_y = r_x = r_y = []
@@ -143,8 +164,7 @@ if __name__ == '__main__':
     ## draw the found points
     targeted[r_y.astype(np.int32), r_x.astype(np.int32)] = [0, 0, 255]
 
-    #plt.imshow(targeted)
-    #plt.show()
+    save_single_image(targeted, 'lanehightlight')
 
     # fit polynomial
     ## left
@@ -160,14 +180,20 @@ if __name__ == '__main__':
     l_fit_x = left_poly(ploty)
     r_fit_x = right_poly(ploty)
 
+    ## create dict for drawing fitted lines on the image
+    fitted_lines = {
+        'l_x': l_fit_x,
+        'r_x': r_fit_x,
+        'ploty': ploty
+    }
 
     ## draw final result
     ## hightlight found points
-    out_img[l_y.astype(np.int32), l_x.astype(np.int32)] = [255, 0, 0]
-    out_img[r_y.astype(np.int32), r_x.astype(np.int32)] = [0, 0, 255]
+    fit_img[l_y.astype(np.int32), l_x.astype(np.int32)] = [255, 0, 0]
+    fit_img[r_y.astype(np.int32), r_x.astype(np.int32)] = [0, 0, 255]
 
     ## draw fitted area
-    window_img = np.zeros_like(out_img)
+    window_img = np.zeros_like(fit_img)
     left_line_window1 = np.array([np.transpose(np.vstack([l_fit_x-margin, ploty]))])
     left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([l_fit_x+margin, ploty])))])
     left_line_pts = np.hstack((left_line_window1, left_line_window2))
@@ -176,16 +202,23 @@ if __name__ == '__main__':
     right_line_pts = np.hstack((right_line_window1, right_line_window2))
     cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
     cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-    plt.imshow(result)
+    fitting_result = cv2.addWeighted(fit_img, 1, window_img, 0.3, 0)
 
-    ## draw fitted lines
-    plt.plot(l_fit_x, ploty, color='yellow')
-    plt.plot(r_fit_x, ploty, color='yellow')
-    
-    ## set up axis
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
-    plt.show()
+    save_single_image(fitting_result, 'fitting_result', fitted_lines)
+
+    # draw fill area crossed by fitted lines and inv-transforming
+    image_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_area = np.dstack((image_zero, image_zero, image_zero))
+    pts_left = np.array([np.flipud(np.transpose(np.vstack([l_fit_x, ploty])))])
+    pts_right = np.array([np.transpose(np.vstack([r_fit_x, ploty]))])
+    pts = np.hstack((pts_left, pts_right))
+    cv2.polylines(color_area, np.int_([pts]), isClosed=False, color=(40, 40, 250), thickness = 50)
+    cv2.fillPoly(color_area, np.int_([pts]), (250, 40, 40))
+    color_area = ptransformer.inv_transform(color_area)
+    save_single_image(color_area, 'inv_transform_color_area')
+
+    # put overlay on original image
+    final_result = cv2.addWeighted(input_image, 1, color_area, 1, 0)
+    save_single_image(final_result, 'final_result')
 
 
